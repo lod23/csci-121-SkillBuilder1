86adb77d1707b36c352b094a9ea609f75d8a3c4a
lod23 <121730996+lod23@users.noreply.github.com>
2025-01-20 01:26:05 -0500

    All edits made

--- src/main/java/SkillBuilder1.java
+++ src/main/java/SkillBuilder1.java
@@ -9,6 +9,21 @@ import java.util.Scanner;
 public class SkillBuilder1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("You're Jane's friend! ");
+
+        System.out.println("What's your name? ");
+        String name = input.nextLine();
+
+        System.out.println("Enter a floating-point number: ");
+
+        float spiceValue = input.nextFloat();
+
+        double result = 4.0 / 3.0 * Math.pow(2, (Math.sqrt(5) / Math.pow(spiceValue, 3)));
+        System.out.println("Well " + name + ", the spice value resulted in " + result);
+
+        double roundedResult = Math.round(result * 100.0) / 100.0;
+        System.out.println("And the converted value is " + roundedResult);
+
         //TODO : Replace this comment with your code
     }
 
@@ -26,6 +41,15 @@ public class SkillBuilder1 {
         // Prompt user to input wall's width
         System.out.println("Enter wall width (feet):");
         double wallWidth = input.nextDouble();
+
+        double area = wallHeight * wallWidth;
+        System.out.println("Wall area: " + area + " square feet");
+
+        double paintNeeded = area / squareFeetPerGallons;
+        System.out.println("Paint needed: " + String.format("%.2f", paintNeeded) + " gallons");
+
+        int cansNeeded = (int) Math.ceil(paintNeeded / gallonsPerCan);  // Round up to the nearest whole number
+        System.out.println("Cans needed: " + cansNeeded + " can(s)");
 
         // TODO: Calculate and output the wall's area
 
